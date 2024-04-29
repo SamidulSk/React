ff79954af0abaa394f14b05867e6f3763d93bd34
SamidulSk <samidulsk@@>
2024-04-29 06:58:11 +0530

    set up vite app

---
+++ vite-app/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1> Learning React | samidul | This is my first react project set up | here i learn how to built a project using react-app and vite bundler | and how to up the server by npm script | and also a little bit about file structing | THANK YOU| </h1>
+  )
+}
+
+export default App
