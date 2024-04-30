4c37460da1a52291f82310bcf2c46faa95f46dc1
SamidulSk <samidulsk@@>
2024-04-30 17:59:38 +0530

    injecting tailwind and basics of props

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-green-500 text-black rounded-xl p-3 font-bold mb-4'>Tailwind Test</h1>
+     <Card username="sk" btnText="visit me"/>
+     <Card username="samanta" btnText="explore me"/>
+     <Card username="grm" />
+    </>
+  )
+}
+
+export default App
