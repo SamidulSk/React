9f3179b099e3d88e0d4612c88b98734327a4b73e
SamidulSk <samidulsk@@>
2024-04-30 14:06:18 +0530

    jsx concept

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,8 +1,11 @@
-
+import Chai from "./chai"
 function App() {
-
+  const username="chai aur code"
   return (
-    <h1> Learning React | samidul | This is my first react project set up | here i learn how to built a project using react-app and vite bundler | and how to up the server by npm script | and also a little bit about file structing | THANK YOU| </h1>
+    <div>
+    <h1> Learning React | samidul {username} | This is my first react project set up | here i learn how to built a project using react-app and vite bundler | and how to up the server by npm script | and also a little bit about file structing | THANK YOU| </h1>
+    <Chai/>
+    </div>
   )
 }
 
